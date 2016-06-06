7d77dd31c42e8d11aede4fdb6e75c58ed29be3c9
sylcastaing <castaing.sylvain@gmail.com>
2016-06-06 17:58:21 +0200

    API test

--- server/api/weather/index.spec.js
+++ server/api/weather/index.spec.js
@@ -3,93 +3,45 @@
 var proxyquire = require('proxyquire').noPreserveCache();
 
 var weatherCtrlStub = {
-  index: 'weatherCtrl.index',
-  show: 'weatherCtrl.show',
-  create: 'weatherCtrl.create',
-  update: 'weatherCtrl.update',
-  destroy: 'weatherCtrl.destroy'
+  search: 'weatherCtrl.search',
+};
+
+var authServiceStub = {
+  isAuthenticated() {
+      return 'authService.isAuthenticated';
+    },
+    hasRole(role) {
+      return 'authService.hasRole.' + role;
+    }
 };
 
 var routerStub = {
   get: sinon.spy(),
-  put: sinon.spy(),
-  patch: sinon.spy(),
-  post: sinon.spy(),
-  delete: sinon.spy()
 };
 
 // require the index with our stubbed out modules
 var weatherIndex = proxyquire('./index.js', {
   'express': {
-    Router: function() {
+    Router: function () {
       return routerStub;
     }
   },
-  './weather.controller': weatherCtrlStub
+  './weather.controller': weatherCtrlStub,
+  '../../auth/auth.service': authServiceStub
 });
 
-describe('Weather API Router:', function() {
+describe('Weather API Router:', function () {
 
-  it('should return an express router instance', function() {
+  it('should return an express router instance', function () {
     expect(weatherIndex).to.equal(routerStub);
   });
 
-  describe('GET /y', function() {
+  describe('GET /y/:start/:end', function () {
 
-    it('should route to weather.controller.index', function() {
+    it('should be authenticated and route to weather.controller.search', function () {
       expect(routerStub.get
-        .withArgs('/', 'weatherCtrl.index')
-        ).to.have.been.calledOnce;
-    });
-
-  });
-
-  describe('GET /y/:id', function() {
-
-    it('should route to weather.controller.show', function() {
-      expect(routerStub.get
-        .withArgs('/:id', 'weatherCtrl.show')
-        ).to.have.been.calledOnce;
-    });
-
-  });
-
-  describe('POST /y', function() {
-
-    it('should route to weather.controller.create', function() {
-      expect(routerStub.post
-        .withArgs('/', 'weatherCtrl.create')
-        ).to.have.been.calledOnce;
-    });
-
-  });
-
-  describe('PUT /y/:id', function() {
-
-    it('should route to weather.controller.update', function() {
-      expect(routerStub.put
-        .withArgs('/:id', 'weatherCtrl.update')
-        ).to.have.been.calledOnce;
-    });
-
-  });
-
-  describe('PATCH /y/:id', function() {
-
-    it('should route to weather.controller.update', function() {
-      expect(routerStub.patch
-        .withArgs('/:id', 'weatherCtrl.update')
-        ).to.have.been.calledOnce;
-    });
-
-  });
-
-  describe('DELETE /y/:id', function() {
-
-    it('should route to weather.controller.destroy', function() {
-      expect(routerStub.delete
-        .withArgs('/:id', 'weatherCtrl.destroy')
-        ).to.have.been.calledOnce;
+        .withArgs('/:start/:end', 'authService.isAuthenticated', 'weatherCtrl.search')
+      ).to.have.been.calledOnce;
     });
 
   });
--- server/api/weather/weather.integration.js
+++ server/api/weather/weather.integration.js
@@ -5,15 +5,17 @@ import request from 'supertest';
 
 var newWeather;
 
-describe('Weather API:', function() {
+describe('Weather API:', function () {
 
-  describe('GET /y', function() {
+  describe('GET /y/:start/:end', function () {
     var weathers;
+    var startDate;
+    var endDate;
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
       request(app)
-        .get('/y')
-        .expect(200)
+        .get('/y/' + startDate + '/' + endDate);
+      .expect(200)
         .expect('Content-Type', /json/)
         .end((err, res) => {
           if (err) {
@@ -24,14 +26,14 @@ describe('Weather API:', function() {
         });
     });
 
-    it('should respond with JSON array', function() {
+    it('should respond with JSON array', function () {
       expect(weathers).to.be.instanceOf(Array);
     });
 
   });
 
-  describe('POST /y', function() {
-    beforeEach(function(done) {
+  describe('POST /y', function () {
+    beforeEach(function (done) {
       request(app)
         .post('/y')
         .send({
@@ -49,17 +51,17 @@ describe('Weather API:', function() {
         });
     });
 
-    it('should respond with the newly created weather', function() {
+    it('should respond with the newly created weather', function () {
       expect(newWeather.name).to.equal('New Weather');
       expect(newWeather.info).to.equal('This is the brand new weather!!!');
     });
 
   });
 
-  describe('GET /y/:id', function() {
+  describe('GET /y/:id', function () {
     var weather;
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
       request(app)
         .get('/y/' + newWeather._id)
         .expect(200)
@@ -73,21 +75,21 @@ describe('Weather API:', function() {
         });
     });
 
-    afterEach(function() {
+    afterEach(function () {
       weather = {};
     });
 
-    it('should respond with the requested weather', function() {
+    it('should respond with the requested weather', function () {
       expect(weather.name).to.equal('New Weather');
       expect(weather.info).to.equal('This is the brand new weather!!!');
     });
 
   });
 
-  describe('PUT /y/:id', function() {
+  describe('PUT /y/:id', function () {
     var updatedWeather;
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
       request(app)
         .put('/y/' + newWeather._id)
         .send({
@@ -96,7 +98,7 @@ describe('Weather API:', function() {
         })
         .expect(200)
         .expect('Content-Type', /json/)
-        .end(function(err, res) {
+        .end(function (err, res) {
           if (err) {
             return done(err);
           }
@@ -105,20 +107,20 @@ describe('Weather API:', function() {
         });
     });
 
-    afterEach(function() {
+    afterEach(function () {
       updatedWeather = {};
     });
 
-    it('should respond with the updated weather', function() {
+    it('should respond with the updated weather', function () {
       expect(updatedWeather.name).to.equal('Updated Weather');
       expect(updatedWeather.info).to.equal('This is the updated weather!!!');
     });
 
   });
 
-  describe('DELETE /y/:id', function() {
+  describe('DELETE /y/:id', function () {
 
-    it('should respond with 204 on successful removal', function(done) {
+    it('should respond with 204 on successful removal', function (done) {
       request(app)
         .delete('/y/' + newWeather._id)
         .expect(204)
@@ -130,7 +132,7 @@ describe('Weather API:', function() {
         });
     });
 
-    it('should respond with 404 when weather does not exist', function(done) {
+    it('should respond with 404 when weather does not exist', function (done) {
       request(app)
         .delete('/y/' + newWeather._id)
         .expect(404)
