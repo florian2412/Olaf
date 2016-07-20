b8abf07d1b466dae183228d5b638ba5e9a9e8186
sylcastaing <castaing.sylvain@gmail.com>
2016-07-20 18:37:53 +0200

    Secure remote API

--- server/api/remote/index.js
+++ server/api/remote/index.js
@@ -1,17 +1,21 @@
 'use strict';
 
-var express = require('express');
-var controller = require('./remote.controller');
+import {
+  Router
+}
+from 'express';
+import * as controller from './remote.controller';
+import * as auth from '../../auth/auth.service';
 
-var router = express.Router();
+var router = new Router();
 
 router.get('/', controller.index);
-router.get('/livebox', controller.liveboxKeys);
+router.get('/livebox', auth.hasRole('admin'), controller.liveboxKeys);
 router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 
 
